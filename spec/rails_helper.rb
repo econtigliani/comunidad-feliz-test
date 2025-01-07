ee7ef8ddbe0d531f7bf9dd71711e906f49931051
emacontigliani <emacontigliani@gmail.com>
2025-01-07 09:59:22 -0300

    fix deprecated warnings with fixture_paths

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,7 +33,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{Rails.root}/spec/fixtures"
+  config.fixture_paths = ["#{Rails.root}/spec/fixtures"]
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -59,11 +59,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  config.include AuthenticationHelper, type: :request
+  # config.include AuthenticationHelper, type: :request
 
   config.before(:all) do
     DatabaseCleaner.start
-    Sidekiq::Worker.clear_all
+    # Sidekiq::Worker.clear_all
   end
 
   config.before(:suite) do
@@ -78,7 +78,6 @@ RSpec.configure do |config|
   end
 
   config.after(:all) do
-    Apartment::Tenant.reset
     DatabaseCleaner.clean
   end
 end
